e34a211bbe6729cb9130a66a0c23655277c8c74f
Jai <jaisch111@gmail.com>
2025-02-01 07:16:14 +0530

    "imporoved morning prac01"

--- sortInWaveForm.cpp
+++ sortInWaveForm.cpp
@@ -32,3 +32,38 @@ int main(){
     return 0;
 }
 
+
+
+// Approach 2:
+// The idea is based on the fact that if we make sure that all even positioned (at index 0, 2, 4, ..) elements are greater than their adjacent odd elements, we don’t need to worry about oddly positioned elements. 
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void sortWaveForm(vector<int> v){
+    
+    for (int i = 0; i < v.size(); i+=2)
+    {
+        // If current even element is smaller than previous
+        if (i>0 && v[i-1] > v[i] )
+            swap(v[i], v[i-1]);
+
+        // If current even element is smaller than next
+        if (i<v.size()-1 && v[i] < v[i+1] )
+            swap(v[i], v[i + 1]);
+    }
+
+    for(auto x: v){
+        cout<<x<<" ";
+    }
+
+}
+
+int main(){
+    vector<int> v = {10, 23, 54, 12, 646, 13, 90};
+
+    sortWaveForm(v);
+
+    return 0;
+}
